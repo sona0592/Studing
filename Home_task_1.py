3009e7d7f105a3be6eca32a63ce8042849094206
sona5002 <sona@example.com>
2023-03-10 17:43:45 +0200

    Home task 1

---
+++ Home_task_1.py
@@ -0,0 +1,26 @@
+text = 'I love Python'
+text *= 42
+print(text)
+
+age_in_month = 30 * 12
+print(age_in_month)
+age_in_years = age_in_month / 12
+print(age_in_years)
+
+my_age = f"My name is Nazar. I'm {int(age_in_years)} years old"
+print(my_age)
+
+x = 1
+y = 3
+print(x > y)
+print(x < y)
+print(x == y)
+print(x != y)
+print(x >= y)
+
+a = '2'
+b = '5'
+c = '6'
+
+d = a + b + c
+print(d)
